d48145d4b1a9c318290f8b1a799a2d21374e6e3c
oathBru <dian@gho.co.za>
2023-07-25 15:34:39 +0200

    First Commit

---
+++ RandomWordsGenerator.py
@@ -0,0 +1,35 @@
+
+
+class RandomWordsGenerator:
+
+    def get_random_words(self):
+        random_words = [
+            "apple", "banana", "orange", "grape", "kiwi",
+            "watermelon", "mango", "strawberry", "blueberry", "raspberry",
+            "pineapple", "pear", "peach", "plum", "apricot",
+            "cherry", "lemon", "lime", "coconut", "pomegranate",
+            "avocado", "melon", "fig", "guava", "papaya",
+            "tangerine", "cantaloupe", "nectarine", "cranberry", "blackberry",
+            "raspberry", "grapefruit", "dragonfruit", "passionfruit", "honeydew",
+            "persimmon", "rhubarb", "lychee", "boysenberry", "gooseberry",
+            "kiwifruit", "mulberry", "quince", "starfruit", "kumquat",
+            "date", "durian", "elderberry", "jamberry", "ackee",
+            "carambola", "feijoa"
+        ]
+        return random_words
+
+    def get_offline_words(self):
+        random_words = [
+            "apple", "banana", "orange", "grape", "kiwi",
+            "watermelon", "mango", "strawberry", "blueberry", "raspberry",
+            "pineapple", "pear", "peach", "plum", "apricot",
+            "cherry", "lemon", "lime", "coconut", "pomegranate",
+            "avocado", "melon", "fig", "guava", "papaya",
+            "tangerine", "cantaloupe", "nectarine", "cranberry", "blackberry",
+            "raspberry", "grapefruit", "dragonfruit", "passionfruit", "honeydew",
+            "persimmon", "rhubarb", "lychee", "boysenberry", "gooseberry",
+            "kiwifruit", "mulberry", "quince", "starfruit", "kumquat",
+            "date", "durian", "elderberry", "jamberry", "ackee",
+            "carambola", "feijoa"
+        ]
+        return random_words
